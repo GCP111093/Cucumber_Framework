681761adf8272ecd97cdab57c8d5d94cc41ebef2
Goutham Kumar Pagadala <gouthamkumarp@outlook.com>
2020-07-08 17:35:04 +0000

    added batch

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-set path=C:\Users\gcp111093\Downloads\apache-maven-3.6.3\bin;C:\Program Files\Java\jdk-13.0.2\bin
+set path=C:\Users\gcp111093\Downloads\apache-maven-3.6.3\bin;C:\Program Files\Java\jdk-13.0.2\bin;C:\Windows\System32;
 
 mvn clean
