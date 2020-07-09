76199e31c998ee937aae83d95297f915f6d205b3
Goutham Kumar Pagadala <gouthamkumarp@outlook.com>
2020-07-09 10:01:31 +0000

    Updated Batch

--- Executor.bat
+++ Executor.bat
@@ -1,3 +1,3 @@
 set path=C:\Users\gcp111093\Downloads\apache-maven-3.6.3\bin;C:\Program Files\Java\jdk-13.0.2\bin;C:\Windows\System32;
 
-mvn test %1 %2 %3
+mvn test -Ddataproviderthreadcount=%1 -Dcucumber.filter.tags=%2 -Dcucumber.plugin=%3
