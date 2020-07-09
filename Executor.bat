ab8b38d947ba2790e79aff21261776e05d56d9e0
Goutham Kumar Pagadala <gouthamkumarp@outlook.com>
2020-07-09 09:24:17 +0000

    Added Batch Executor

---
+++ Executor.bat
@@ -0,0 +1,6 @@
+@echo off
+echo %1
+echo %2
+echo %3
+
+mvn test %1 %2 %3
--- sanity-api.bat
+++ sanity-api.bat
@@ -1,3 +1,3 @@
 set path=C:\Users\gcp111093\Downloads\apache-maven-3.6.3\bin;C:\Program Files\Java\jdk-13.0.2\bin;C:\Windows\System32;
 
-mvn install -Dcucumber.filter.tags="@sanity"
+mvn install -DDcucumber.options="--tags @sanity --plugin json:report/cucumber.json --plugin junit:target/junitreports.xml"
